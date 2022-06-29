2527bfc1df4a6b041afdd7ef05be4e89d4c6ce2d
Timur <tumyp.paxumob@gmail.com>
2022-06-30 00:42:16 +0500

    Added Task 4 from previous Homework

---
+++ Task_1.4/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Input number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 0)
+        {
+            for (int current = 2; current <= number; current = current + 2)
+                Console.WriteLine(current + " ");
+        }
+        else
+        {
+            for (int current = -2; current >= number; current = current - 2)
+                Console.WriteLine(current + " ");
+        }
+    }
+}
